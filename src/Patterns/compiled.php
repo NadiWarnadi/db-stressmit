79534b2ab54cee540d1c6f55292cefad188ef34d
NadiWarnadi <182494866+NadiWarnadi@users.noreply.github.com>
2026-06-24 02:42:35 +0000

    chore(security): auto-update dynamic exploit payloads from OSV [skip ci]

--- src/Patterns/compiled.php
+++ src/Patterns/compiled.php
@@ -6,7 +6,7 @@
  */
 
 return [
-    'generated_at' => '2026-06-23 02:41:47',
+    'generated_at' => '2026-06-24 02:42:35',
     'dynamic_regex' => '/\\b(sleep|benchmark|extractvalue|updatexml|load_file|union|select)\\b\\s*(\\(|\\[|--|\\#|\\/\\*|\\s|$)/i',
     'keywords' => array (
   0 => 'sleep',
